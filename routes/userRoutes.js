184b5d905c151ca1fb350767247df2c0354282dc
Chavamora <sbetomora@gmail.com>
2021-01-17 17:22:17 -0600

    d

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 module.exports = app => {
     app.route('/users/login')
     .get(userController.getLogin)
-    .post(userController.login),
+    .post(userController.login)
     app.route('/users/register')
     .get(userController.getRegister)
     .post(userController.register)
